7f7ee3176fe0de453147ad4dd683e1bba92b016f
Eric Charles <badass@stanford.edu>
2026-06-04 18:39:02 -0700

    move download area

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,8 @@ import pytest
 from pz_data_challenge import submit_utils
 
 # don't change these
-PUBLIC_URL: str = "https://portal.nersc.gov/cfs/lsst/PZ/data_challenge/public_test.tgz"
+#PUBLIC_URL: str = "https://portal.nersc.gov/cfs/lsst/PZ/data_challenge/public_test.tgz"
+PUBLIC_URL: str = "https://s3df.slac.stanford.edu/people/echarles/public_test.tgz"
 
 
 @pytest.fixture(name="setup_public_area", scope="package")
