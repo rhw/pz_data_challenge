fc51fd0d6859e1036f575b307265f07b094e8710
Eric Charles <badass@stanford.edu>
2026-06-30 17:15:27 -0700

    fix public url

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from pz_data_challenge import submit_utils
 
 # don't change these
-PUBLIC_URL: str = "https://portal.nersc.gov/cfs/lsst/PZ/data_challenge/public_test.tgz"
+PUBLIC_URL: str = "https://portal.nersc.gov/cfs/lsst/PZ/data_challenge/public.tgz"
 
 
 @pytest.fixture(name="setup_public_area", scope="package")
